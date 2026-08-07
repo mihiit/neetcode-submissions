2e1a5044267501c1c8326d9f35b03b2dea5a00bf
Mihit Nanda <mihitnanda2005@gmail.com>
2026-08-07 10:16:16 +0530

    Update: concatenation-of-array - submission-1

--- Data Structures & Algorithms/concatenation-of-array/submission-1.py
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -1,7 +1,6 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-        for n in range(2):
-            for n in nums:
-                ans.append(n)
-        return ans
+        return nums + nums
+        
+        
+        
